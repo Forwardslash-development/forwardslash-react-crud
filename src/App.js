d7aca38ce19dec4c396895d2537593e001acf643
Forwardslash-development <forwardslash.development@gmail.com>
2021-07-28 15:27:34 -0500

    updated components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Create from './components/crud/create';
+import Read from './components/crud/read';
+import Update from './components/crud/update';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <div className="main">
      <h2 className="main-header">Forwardslash React CRUD</h2>
      <div>
-       <Create/>
+     <Route exact path='/create' component={Create} />
      </div>
+     <div style={{ marginTop: 20 }}>
+          <Route exact path='/read' component={Read} />
+        </div>
+        <Route path='/update' component={Update} />
     </div>
+    </Router>
   );
 }
 
--- src/components/crud/create.js
+++ src/components/crud/create.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { useHistory } from 'react-router';
 
-const Create = () => (
-  <Form className="create-form">
-    <Form.Field>
-      <label>First Name</label>
-      <input placeholder='First Name' />
-    </Form.Field>
-    <Form.Field>
-      <label>Last Name</label>
-      <input placeholder='Last Name' />
-    </Form.Field>
-    <Form.Field>
-      <Checkbox label='I agree to the Terms and Conditions' />
-    </Form.Field>
-    <Button type='submit'>Submit</Button>
-  </Form>
-)
+export default function Create() {
+    let history = useHistory();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [checkbox, setCheckbox] = useState(false);
 
-export default Create;
+    const postData = () => {
+      axios.post('https://6101a72249a537001787124e.mockapi.io/api/v1/forwardslashData', {
+      firstName,
+      lastName,
+      checkbox
+    }).then(() => {
+        history.push('/read')
+    })
+  }
+    return (
+        <div>
+            <Form className="create-form">
+                <Form.Field>
+                    <label>First Name</label>
+                    <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)} />
+                </Form.Field>
+                <Button onClick={postData} type='submit'>Submit</Button>
+            </Form>
+        </div>
+    )
+}
